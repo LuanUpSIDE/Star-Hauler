f5f5ebba39bbe320a5379a85eb4f27d9b8ed95d1
Luan Xavier <115136809+LuanUpSIDE@users.noreply.github.com>
2025-08-24 02:32:13 +0000

    00000

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,11 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANTE: Defina a base para o nome do seu repositório
+  // Esta linha DEVE corresponder ao nome do seu repositório no GitHub
   base: "/Star-Hauler/", 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> a2ee27475d736902d3bfc785125b51fe1d9985b4
